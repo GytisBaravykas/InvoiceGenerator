c0d17fe5adcfa8532066108e1ae19fc4ca1047ee
G B <gytero2@gmail.com>
2018-11-19 02:48:50 +0200

    Fixed REST operations.

--- InvoiceGenerator/Controllers/InvoiceController.cs
+++ InvoiceGenerator/Controllers/InvoiceController.cs
@@ -34,32 +34,64 @@ namespace InvoiceGenerator.Controllers
 
         // GET: api/Invoice/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
+            var invoice = _context.Invoices // it works
+                .Include(c => c.Service)
+                .Include(c => c.ServiceProvider)
+                .Include(c => c.Client)
+                .Include(c => c.ServiceProvider.Country)
+                .Include(c => c.Client.Country).SingleOrDefault(i=>i.Id == id);
+
+            return Ok(invoice);
         }
 
         // POST: api/Invoice
         [HttpPost]
         public void CreateInvoice(Invoice invoice)
         {
-
+            invoice.ServiceProvider.VatPayerState(invoice);
+            invoice.Client.VatPayerState(invoice);
             invoice.Service.VatCalculator(invoice);
             invoice.Service.WholePriceCalculator(invoice);
+
             _context.Invoices.Add(invoice);
             _context.SaveChanges();
         }
 
         // PUT: api/Invoice/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, Invoice invoice)  
         {
+            var invoiceDb = _context.Invoices
+                .Include(c => c.Service)
+                .Include(c => c.ServiceProvider)
+                .Include(c => c.Client)
+                .Include(c => c.ServiceProvider.Country)
+                .Include(c => c.Client.Country).SingleOrDefault(i => i.Id == id);
+
+            invoiceDb.Date = invoice.Date;
+            invoiceDb.Client = invoice.Client;
+            invoiceDb.ServiceProvider = invoice.ServiceProvider;
+            invoiceDb.Service = invoice.Service;
+
+            _context.Update(invoiceDb);
+            _context.SaveChanges();
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(int id)  // works
         {
+            var invoiceDb = _context.Invoices
+                .Include(c => c.Service)
+                .Include(c => c.ServiceProvider)
+                .Include(c => c.Client)
+                .Include(c => c.ServiceProvider.Country)
+                .Include(c => c.Client.Country).SingleOrDefault(i => i.Id == id);
+
+            _context.Remove(invoiceDb);
+            _context.SaveChanges();
         }
     }
 }
